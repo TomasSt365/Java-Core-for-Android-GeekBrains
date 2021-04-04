2bab6b48ca5d7be2bdf9b1c0894061744bea1afa
Tomas.St <lapink540@gmail.com>
2021-04-04 14:02:08 +0300

    the Person class is written and ready to use

--- src/Lessons/Lesson5/Person.java
+++ src/Lessons/Lesson5/Person.java
@@ -2,4 +2,83 @@ package Lessons.Lesson5;
 
 public class Person {
 
+    private  String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Person(String fullName, String position,
+                  String email, String phoneNumber,
+                  int salary, int age){
+
+        setFullName(fullName);
+        setPosition(position);
+        setEmail(email);
+        setPhoneNumber(phoneNumber);
+        setSalary(salary);
+        setAge(age);
+
+    }
+
+    public void Info(){
+
+        System.out.println("Name: " + getFullName());
+        System.out.println("Position: " + getPosition());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Phone number: " + getPhoneNumber());
+        System.out.println("Salary" + getSalary());
+        System.out.println("Age: " + getAge());
+
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
 }

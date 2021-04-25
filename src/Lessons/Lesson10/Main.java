57c8155d070fe85aa00be2a16c898acae9724229
Tomas <lapink540@gmail.com>
2021-04-25 16:46:45 +0300

    The second task is completed

--- src/Lessons/Lesson10/Main.java
+++ src/Lessons/Lesson10/Main.java
@@ -1,27 +1,31 @@
 package Lessons.Lesson10;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<String> ListArr = new ArrayList<>(Arrays.asList
+        List<String> listArr = new ArrayList<>(Arrays.asList
                 ("1","2","3","10","23","23","12","43","12","2"));
-        /**
-         * I used ArrayList because
-         * I don't need to constantly change my list, but only get elements from it
-         */
 
-        PrintUniqueElements(ListArr);
-        PrintElementsAndTheirNumber(ListArr);
+        PrintUniqueElements(listArr);
+        PrintElementsAndTheirNumber(listArr);
+
+        Map<String, String> phonebook = new HashMap<>();
+        phonebook.put("124325423","Janovich");
+        phonebook.put("243552155225","Demidov");
+        phonebook.put("44352343532", "Vasilev");
+        phonebook.put("234435", "Demidov");
+        phonebook.put("332435445", "Janovich");
+
+        PrintNumbersBySearchingLastName(phonebook,"Janovich");
 
     }
 
     public static void PrintUniqueElements(List<String> list){
+
         for (String element : list) {
             if(getNumberOfEqualsElementsOfList(element, list) == 1){
                 System.out.println(element);
@@ -50,6 +54,23 @@ public class Main {
         }
         return NumberOfEqualsElements;
     }
+
+    public static void PrintNumbersBySearchingLastName
+            (Map<String, String> phonebook, String searchingLastName) {
+        int numberOfPrints = 0;
+        Set<Map.Entry<String, String>> entries = phonebook.entrySet();
+
+        for (Map.Entry<String, String> entry : entries) {
+            if (entry.getValue().equalsIgnoreCase(searchingLastName)) {
+                System.out.println(entry.getKey());
+                numberOfPrints++;
+            }
+        }
+        if (numberOfPrints == 0) {
+            System.out.println("No results were found for your search");
+        }
+    }
+
 }
 
 

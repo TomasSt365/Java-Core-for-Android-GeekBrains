72d972e89fe422b94389efeb9ba75c0cdd521379
TomasSt365 <lapink365@gmail.com>
2022-10-29 20:00:09 +0200

    code simplification

--- src/Lessons/Lesson10/Main.java
+++ src/Lessons/Lesson10/Main.java
@@ -8,29 +8,29 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> listArr = new ArrayList<>(Arrays.asList
-                ("1","2","3","10","23","23","12","43","12","2"));
+                ("1", "2", "3", "10", "23", "23", "12", "43", "12", "2"));
 
         PrintUniqueElements(listArr);
         PrintElementsAndTheirNumber(listArr);
 
         Map<String, String> phonebook = new HashMap<>();
-        phonebook.put("124325423","Janovich");
-        phonebook.put("243552155225","Demidov");
+        phonebook.put("124325423", "Janovich");
+        phonebook.put("243552155225", "Demidov");
         phonebook.put("44352343532", "Vasilev");
         phonebook.put("234435", "Demidov");
         phonebook.put("332435445", "Janovich");
 
-        PrintNumbersBySearchingLastName(phonebook,"Janovich");
+        PrintNumbersBySearchingLastName(phonebook, "Janovich");
 
     }
 
-    public static void PrintUniqueElements(List<String> list){
+    public static void PrintUniqueElements(List<String> list) {
         Set<String> set = new HashSet<>(list);
         System.out.println(Arrays.toString(set.toArray()));
     }
 
-    public static void PrintElementsAndTheirNumber(List<String> list){
-        for (String element : list){
+    public static void PrintElementsAndTheirNumber(List<String> list) {
+        for (String element : list) {
             System.out.println("(Element;Number): (" + element + ";" +
                     getNumberOfEqualsElementsOfList(element, list) + ")");
         }
@@ -41,9 +41,9 @@ public class Main {
 
         int numberOfEqualsElements = 0;
 
-        for (String element : list){
+        for (String element : list) {
 
-            if(element.equals(checkedElement)){
+            if (element.equals(checkedElement)) {
                 numberOfEqualsElements++;
             }
 
@@ -54,14 +54,14 @@ public class Main {
     public static void PrintNumbersBySearchingLastName
             (Map<String, String> phonebook, String searchingLastName) {
         boolean isNotFound = true;
-        Set<Map.Entry<String, String>> entries = phonebook.entrySet();
 
-        for (Map.Entry<String, String> entry : entries) {
+        for (Map.Entry<String, String> entry : phonebook.entrySet()) {
             if (entry.getValue().equalsIgnoreCase(searchingLastName)) {
                 System.out.println(entry.getKey());
                 isNotFound = false;
             }
         }
+
         if (isNotFound) {
             System.out.println("No results were found for your search");
         }

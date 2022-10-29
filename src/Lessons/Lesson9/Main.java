08df62bf5128b6b1f616ebe3ba854332a8661780
TomasSt365 <lapink365@gmail.com>
2022-10-29 16:09:27 +0200

    Correction of remarks of lesson 9

--- src/Lessons/Lesson9/Main.java
+++ src/Lessons/Lesson9/Main.java
@@ -2,12 +2,12 @@ package Lessons.Lesson9;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] Arr = {{"5","6","12","11"},{"7","8","5","11"},{"7","10","5","11"},{"11","12","5","11"}};
+        String[][] Arr = {{"5", "6", "12","10"}, {"7", "8", "5","6"}, {"7", "10", "5","4"}, {"11", "12", "5","7"}};
 
         try {
             System.out.println(SumArrayElements(Arr));
         } catch (MyArraySizeException | MyArrayDataException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
     }
@@ -15,15 +15,21 @@ public class Main {
     public static int SumArrayElements(String[][] elements) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
 
-        if(elements.length !=  4){
-            throw new MyArraySizeException("Wrong field size!");
+        if (elements.length == 4) {
+            for (int i = 0; i < elements.length; i++) {
+                if (elements[i].length != 4) {
+                    throw new MyArraySizeException("Wrong size! Element[" + i + "] don't have 4 elements");
+                }
+            }
+        } else {
+            throw new MyArraySizeException("Wrong size! Array length don't have 4 elements");
         }
 
         for (int i = 0; i < elements.length; i++) {
-            for (int j = 0; j < elements.length; j++) {
-                try{
+            for (int j = 0; j < elements[0].length; j++) {
+                try {
                     sum += Integer.parseInt(elements[i][j]);
-                }catch (NumberFormatException e1){
+                } catch (NumberFormatException e1) {
                     throw new MyArrayDataException("element[" + i + "][" + j + "] is not number");
                 }
             }
